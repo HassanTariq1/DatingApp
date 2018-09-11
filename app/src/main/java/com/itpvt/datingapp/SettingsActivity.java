847c5bb995b043bf69904a193990767a36b4e106
Hassan <mughalhassan467@gmail.com>
2018-09-11 16:47:45 -0700

    third   one

--- app/src/main/java/com/itpvt/datingapp/SettingsActivity.java
+++ app/src/main/java/com/itpvt/datingapp/SettingsActivity.java
@@ -35,201 +35,159 @@ import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private EditText mNamee,mPh;
-    private Button cnfrm, back;
+
+    private EditText mNameField, mPhoneField;
+
+    private Button mBack, mConfirm;
 
     private ImageView mProfileImage;
-private FirebaseAuth auth;
-private DatabaseReference datacus;
 
-private   String userId, name, profileImageUrl, phone;
-private Uri resulturi;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mCustomerDatabase;
+
+    private String userId, name, phone, profileImageUrl;
+
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-mNamee=(EditText) findViewById(R.id.namee);
-        mPh=(EditText) findViewById(R.id.phone);
-        cnfrm=(Button) findViewById(R.id.conf);
-        back=(Button) findViewById(R.id.back);
-        mProfileImage=(ImageView)findViewById(R.id.profile);
-
-
-        String usersex= getIntent().getExtras().getString("userSex");
-
-        auth=FirebaseAuth.getInstance();
-        userId= auth.getCurrentUser().getUid();
-
-        datacus= FirebaseDatabase.getInstance().getReference().child("Users").child(usersex).child(userId);
-
-   getUserInfo();
-   mProfileImage.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View view) {
-
-Intent i= new Intent(Intent.ACTION_PICK);
-i.setType("image/*");
-startActivityForResult(i,1);
+        String userSex = getIntent().getExtras().getString("userSex");
 
 
 
-       }
-   });
+        mNameField = (EditText) findViewById(R.id.namee);
+        mPhoneField = (EditText) findViewById(R.id.phone);
 
+        mProfileImage = (ImageView) findViewById(R.id.profile);
 
-        cnfrm.setOnClickListener(new View.OnClickListener() {
+        mBack = (Button) findViewById(R.id.back);
+        mConfirm = (Button) findViewById(R.id.conf);
+
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userSex).child(userId);
+
+        getUserInfo();
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-saveInfo();
-
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
             }
         });
-
-        back.setOnClickListener(new View.OnClickListener() {
+        mConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                saveUserInformation();
+            }
+        });
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 finish();
                 return;
+            }
+        });
+    }
 
+
+    private void getUserInfo() {
+        mCustomerDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("name")!=null){
+                        name = map.get("name").toString();
+                        mNameField.setText(name);
+                    }
+                    if(map.get("phone")!=null){
+                        phone = map.get("phone").toString();
+                        mPhoneField.setText(phone);
+                    }
+
+                    if(map.get("profileImageUrl")!=null){
+                        profileImageUrl = map.get("profileImageUrl").toString();
+                        switch(profileImageUrl){
+                            case "default":
+                                Glide.with(getApplication()).load(R.mipmap.ic_launcher).into(mProfileImage);
+                                break;
+                            default:
+                                Glide.with(getApplication()).load(profileImageUrl).into(mProfileImage);
+                                break;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
 
-
-
     }
 
-    private  void getUserInfo(){
+    private void saveUserInformation() {
+        name = mNameField.getText().toString();
+        phone = mPhoneField.getText().toString();
 
-        datacus.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-if (dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0){
-
-Map<String, Object> map= ( Map<String,Object >) dataSnapshot.getValue();
-
-        if (map.get("name")!=null){
-
-
-        name= map.get("name").toString();
-        mNamee.setText(name);
-
-
-    }
-    if (map.get("phone")!=null){
-
-
-        phone= map.get("phone").toString();
-        mPh.setText(phone);
-
-
-    }
-    if (map.get("profileImageUrl")!=null){
-
-
-        profileImageUrl= map.get("profileImageUrl").toString();
-        Glide.with(getApplication()).load(profileImageUrl).into(mProfileImage);
-
-
-
-    }
-
-}
-
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-
-
-    }
-
-    private void saveInfo() {
-
-    name=mNamee.getText().toString();
-    phone= mPh.getText().toString();
-
-        Map userInfo= new HashMap();
-        userInfo.put("name",name);
-        userInfo.put("phone",phone);
-
-
-        datacus.updateChildren(userInfo);
-
-        if(resulturi !=null){
-
-
-            StorageReference filepath= FirebaseStorage.getInstance().getReference().child("profileImages").child(userId);
-
-            Bitmap bit= null;
+        Map userInfo = new HashMap();
+        userInfo.put("name", name);
+        userInfo.put("phone", phone);
+        mCustomerDatabase.updateChildren(userInfo);
+        if(resultUri != null){
+            StorageReference filepath = FirebaseStorage.getInstance().getReference().child("profileImages").child(userId);
+            Bitmap bitmap = null;
 
             try {
-                bit= MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(),resulturi);
+                bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-
-
-            ByteArrayOutputStream boas= new ByteArrayOutputStream();
-            bit.compress(Bitmap.CompressFormat.JPEG, 20, boas);
-byte[] data= boas.toByteArray();
-            UploadTask upload= filepath.putBytes(data);
-
-            upload.addOnFailureListener(new OnFailureListener() {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = filepath.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
                     finish();
-
                 }
             });
-
-            upload.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
 
-                    Uri down= taskSnapshot.getDownloadUrl();
+                    Map userInfo = new HashMap();
+                    userInfo.put("profileImageUrl", downloadUrl.toString());
+                    mCustomerDatabase.updateChildren(userInfo);
 
-                    Map userInfo= new HashMap();
-                    userInfo.put("profileImageUrl",down.toString());
-
-                    datacus.updateChildren(userInfo);
-finish();
-return;
+                    finish();
+                    return;
                 }
             });
 
-
-
         }else{
-
             finish();
         }
-
-
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==1 && resultCode== Activity.RESULT_OK   ){
-
-            final Uri   imguri= data.getData();
-            resulturi= imguri;
-            mProfileImage.setImageURI(resulturi);
-
-
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            mProfileImage.setImageURI(resultUri);
         }
     }
+
 }
